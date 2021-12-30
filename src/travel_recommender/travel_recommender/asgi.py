661f542d7144d1d748bba6cd0ba8e992a9067061
IsaacCheng9 <IsaacCheng9@users.noreply.github.com>
2021-12-30 23:26:23 +0000

    Format code with Black and Prettier

--- src/travel_recommender/travel_recommender/asgi.py
+++ src/travel_recommender/travel_recommender/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travel_recommender.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel_recommender.settings")
 
 application = get_asgi_application()
