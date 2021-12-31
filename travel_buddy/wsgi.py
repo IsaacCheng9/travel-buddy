18f6d4cc6be25e89d51864b0af4ae27c58398018
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2021-12-31 18:05:01 +0000

    Fix project structure (#9)
    * Simplify project structure
    
    * Rename database
    
    * Remove target path of src
    
    * Fix Black formatter target path

--- travel_buddy/wsgi.py
+++ travel_buddy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for travel_recommender project.
+WSGI config for travel_buddy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "travel_recommender.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travel_buddy.settings')
 
 application = get_wsgi_application()
