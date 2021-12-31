35d968a2383d70a0f87ae1cbe193d6ed6e75def5
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2021-12-31 20:41:27 +0000

    Create view for registration page (#11)
    * Create forms for registration and updating email
    
    * Create view for user registration
    
    * Fix view for register page
    
    * Format code with Black and Prettier
    
    Co-authored-by: IsaacCheng9 <IsaacCheng9@users.noreply.github.com>

--- travel_buddy/settings.py
+++ travel_buddy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "user.apps.UserConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en-gb"
 
 TIME_ZONE = "UTC"
 
--- travel_buddy/urls.py
+++ travel_buddy/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from user import views as user_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", user_views.register, name="register"),
 ]
