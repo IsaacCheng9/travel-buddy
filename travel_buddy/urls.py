534998122d0c36259cb6e54b981191120ae37847
IsaacCheng9 <isaaccheng9@gmail.com>
2021-12-31 17:47:10 +0000

    Simplify project structure

--- travel_buddy/urls.py
+++ travel_buddy/urls.py
@@ -1,4 +1,4 @@
-"""travel_recommender URL Configuration
+"""travel_buddy URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
