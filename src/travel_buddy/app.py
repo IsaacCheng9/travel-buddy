bb7b41350516fd31a1f155fb8940c6d21c13eb78
IsaacCheng9 <isaaccheng9@gmail.com>
2022-01-02 00:31:26 +0000

    Add app run, add app secret key

--- src/travel_buddy/app.py
+++ src/travel_buddy/app.py
@@ -5,8 +5,15 @@ import travel_buddy.views.register as register
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(register.register_blueprint, url_prefix="")
+app.secret_key = (
+    "i09kd0owe671gdyfjkzmpaopmrionfkwep]l[';fkvfoiejiojd1u232e23fkeorincuiwkdu"
+)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
