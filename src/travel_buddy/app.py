be85e1c9fae969da988b0ae7470fe4031893249c
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-01-02 01:03:56 +0000

    Add login system (#15)
    * Add register blueprint
    
    * Add names to input forms
    
    * Add username instead of user_id
    
    * Remove email column
    
    * Add register helper
    
    * Add register page to Flask
    
    * Reorder first name and last name in account table
    
    * Fix form action, remove Django csrf_token
    
    * Add form action, remove Django csrf_token
    
    * Add app run, add app secret key
    
    * Fix register route methods
    
    * Add DB path
    
    * Change email field to username field
    
    * Add sample user 'isaaccheng9'
    
    * Change email field to username field
    
    * Add login page to Flask
    
    * Rename variable for clarity

--- src/travel_buddy/app.py
+++ src/travel_buddy/app.py
@@ -3,13 +3,16 @@ import os
 from flask import Flask, request, session
 
 import travel_buddy.views.register as register
+import travel_buddy.views.login as login
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "db.sqlite3")
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 app.register_blueprint(register.register_blueprint, url_prefix="")
+app.register_blueprint(login.login_blueprint, url_prefix="")
+
+app.url_map.strict_slashes = False
 app.secret_key = (
     "i09kd0owe671gdyfjkzmpaopmrionfkwep]l[';fkvfoiejiojd1u232e23fkeorincuiwkdu"
 )
---
+++ src/travel_buddy/views/login.py
@@ -0,0 +1,65 @@
+"""
+Handles the view for the login system and related functionality.
+"""
+
+import sqlite3
+
+from flask import Blueprint, redirect, render_template, request, session
+from passlib.hash import sha256_crypt
+
+login_blueprint = Blueprint(
+    "login", __name__, static_folder="static", template_folder="templates"
+)
+
+
+@login_blueprint.route("/login", methods=["GET", "POST"])
+def login_page() -> object:
+    """
+    Renders the login page and validates the user's login details.
+
+    Returns:
+         GET: The web page for user login.
+         POST: Redirection depending on whether login was successful or not.
+    """
+    if request.method == "GET":
+        errors = []
+        if "username" in session:
+            return redirect("/profile")
+        else:
+            if "error" in session:
+                errors = session["error"]
+            session["prev-page"] = request.url
+            # Clear error session variables.
+            session.pop("error", None)
+            return render_template("login.html", errors=errors)
+
+    elif request.method == "POST":
+        username = request.form["username"].lower()
+        password = request.form["password"]
+
+        with sqlite3.connect("db.sqlite3") as conn:
+            cur = conn.cursor()
+            # Gets user from database using username.
+            cur.execute(
+                "SELECT password FROM account WHERE username=?;", (username,)
+            )
+            conn.commit()
+            row = cur.fetchone()
+
+        if row:
+            hashed_password = row[0]
+        else:
+            session["error"] = ["login"]
+            return redirect("/login")
+
+        if hashed_password:
+            if sha256_crypt.verify(password, hashed_password):
+                session["username"] = username
+                session["prev-page"] = request.url
+                return redirect("/profile")
+            else:
+                session["error"] = ["login"]
+                return redirect("/login")
+        else:
+            session["error"] = ["login"]
+            return redirect("/login")
