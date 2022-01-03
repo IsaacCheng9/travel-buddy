5eb9ddb88abcd23bcaa74282a7f3bb36a26e46ca
IsaacCheng9 <isaaccheng9@gmail.com>
2022-01-03 03:21:55 +0000

    Change login route to "/"

--- src/travel_buddy/app.py
+++ src/travel_buddy/app.py
@@ -2,8 +2,8 @@ import os
 
 from flask import Flask, request, session
 
-import travel_buddy.views.register as register
 import travel_buddy.views.login as login
+import travel_buddy.views.register as register
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "db.sqlite3")
@@ -16,12 +16,6 @@ app.url_map.strict_slashes = False
 app.secret_key = (
     "i09kd0owe671gdyfjkzmpaopmrionfkwep]l[';fkvfoiejiojd1u232e23fkeorincuiwkdu"
 )
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
 
 if __name__ == "__main__":
     app.run(debug=True)
