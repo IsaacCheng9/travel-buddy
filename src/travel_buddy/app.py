a33c3d4f393a0206d6c55879580c5c1566bd2e80
IsaacCheng9 <isaaccheng9@gmail.com>
2022-01-03 16:26:05 +0000

    Add redirection to profile page after login/registration

--- src/travel_buddy/app.py
+++ src/travel_buddy/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, session
 
 import travel_buddy.views.login as login
 import travel_buddy.views.register as register
+import travel_buddy.views.profile as profile
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "db.sqlite3")
@@ -11,6 +12,7 @@ DB_PATH = os.path.join(BASE_DIR, "db.sqlite3")
 app = Flask(__name__)
 app.register_blueprint(register.register_blueprint, url_prefix="")
 app.register_blueprint(login.login_blueprint, url_prefix="")
+app.register_blueprint(profile.profile_blueprint, url_prefix="")
 
 app.url_map.strict_slashes = False
 app.secret_key = (
