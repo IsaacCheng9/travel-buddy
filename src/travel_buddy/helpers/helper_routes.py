8979817c9c30cdd7bab511cfb7db90ed96c6d849
BarnabasG <BarnabasG@users.noreply.github.com>
2022-01-18 00:38:26 +0000

    Format Python code with Black

--- src/travel_buddy/helpers/helper_routes.py
+++ src/travel_buddy/helpers/helper_routes.py
@@ -4,13 +4,15 @@ import googlemaps
 from time import sleep
 from base64 import b64decode
 
+
 def get_keys(filename):
-    filename = 'keys.json'
-    with open(filename, 'r') as f:
+    filename = "keys.json"
+    with open(filename, "r") as f:
         keys = json.loads(f.read())
-    keys = {k: b64decode(v.encode()).decode() for (k,v) in keys.items()}
+    keys = {k: b64decode(v.encode()).decode() for (k, v) in keys.items()}
 
     return keys
+
 
 def generate_client(api_key):
     return googlemaps.Client(api_key)
@@ -18,24 +20,17 @@ def generate_client(api_key):
 
 def run_api(map_client, origins, destinations, mode):
 
-    response = map_client.distance_matrix(
-        origins,
-        destinations,
-        mode=mode
-    )
+    response = map_client.distance_matrix(origins, destinations, mode=mode)
 
-    next_page_token = response.get('next_page_token')
+    next_page_token = response.get("next_page_token")
 
     while next_page_token:
         sleep(2)
-        response = map_client.distance_matrix(
-            origins,
-            destinations,
-            mode=mode
-        )
-        next_page_token = response.get('next_page_token')
-    
+        response = map_client.distance_matrix(origins, destinations, mode=mode)
+        next_page_token = response.get("next_page_token")
+
     return response
+
 
 def safeget(dct, *keys):
     for key in keys:
@@ -45,6 +40,7 @@ def safeget(dct, *keys):
             return None
     return dct
 
-#TODO
+
+# TODO
 def validate_address(adr):
     pass
--- src/travel_buddy/views/routes.py
+++ src/travel_buddy/views/routes.py
@@ -9,46 +9,77 @@ routes_blueprint = Blueprint(
     "routes", __name__, static_folder="static", template_folder="templates"
 )
 
+
 @routes_blueprint.route("/routes", methods=["GET", "POST"])
 def routes() -> object:
     if request.method == "GET":
-        return render_template(
-            "routes.html",
-            distance_range = "",
-            details = {}
-        )
+        return render_template("routes.html", distance_range="", details={})
 
     elif request.method == "POST":
         origins = request.form["start_point"]
         destinations = request.form["destination"]
         mode = request.form["mode"]
-        print(origins,destinations,mode)
+        print(origins, destinations, mode)
 
-        filename = 'keys.json'
+        filename = "keys.json"
         KEYS = helper_routes.get_keys(filename)
-        map_client = helper_routes.generate_client(KEYS['google_maps'])
+        map_client = helper_routes.generate_client(KEYS["google_maps"])
         route_data = {}
-        route_data['walking'] = helper_routes.run_api(map_client, origins, destinations, 'walking')
+        route_data["walking"] = helper_routes.run_api(
+            map_client, origins, destinations, "walking"
+        )
         print("route", route_data)
-        route_data['driving'] = helper_routes.run_api(map_client, origins, destinations, 'driving')
-        route_data['cycling'] = helper_routes.run_api(map_client, origins, destinations, 'bicycling')
-        route_data['transit'] = helper_routes.run_api(map_client, origins, destinations, 'transit')
+        route_data["driving"] = helper_routes.run_api(
+            map_client, origins, destinations, "driving"
+        )
+        route_data["cycling"] = helper_routes.run_api(
+            map_client, origins, destinations, "bicycling"
+        )
+        route_data["transit"] = helper_routes.run_api(
+            map_client, origins, destinations, "transit"
+        )
 
         details = {
-            'walking': {'distance':helper_routes.safeget(route_data,'walking','rows',0,'elements',0,'distance'),'duration':helper_routes.safeget(route_data,'walking','rows',0,'elements',0,'duration')},
-            'driving': {'distance':helper_routes.safeget(route_data,'driving','rows',0,'elements',0,'distance'),'duration':helper_routes.safeget(route_data,'driving','rows',0,'elements',0,'duration')},
-            'cycling': {'distance':helper_routes.safeget(route_data,'cycling','rows',0,'elements',0,'distance'),'duration':helper_routes.safeget(route_data,'cycling','rows',0,'elements',0,'duration')},
-            'transit': {'distance':helper_routes.safeget(route_data,'transit','rows',0,'elements',0,'distance'),'duration':helper_routes.safeget(route_data,'transit','rows',0,'elements',0,'duration')}
+            "walking": {
+                "distance": helper_routes.safeget(
+                    route_data, "walking", "rows", 0, "elements", 0, "distance"
+                ),
+                "duration": helper_routes.safeget(
+                    route_data, "walking", "rows", 0, "elements", 0, "duration"
+                ),
+            },
+            "driving": {
+                "distance": helper_routes.safeget(
+                    route_data, "driving", "rows", 0, "elements", 0, "distance"
+                ),
+                "duration": helper_routes.safeget(
+                    route_data, "driving", "rows", 0, "elements", 0, "duration"
+                ),
+            },
+            "cycling": {
+                "distance": helper_routes.safeget(
+                    route_data, "cycling", "rows", 0, "elements", 0, "distance"
+                ),
+                "duration": helper_routes.safeget(
+                    route_data, "cycling", "rows", 0, "elements", 0, "duration"
+                ),
+            },
+            "transit": {
+                "distance": helper_routes.safeget(
+                    route_data, "transit", "rows", 0, "elements", 0, "distance"
+                ),
+                "duration": helper_routes.safeget(
+                    route_data, "transit", "rows", 0, "elements", 0, "duration"
+                ),
+            },
         }
         print(details)
 
-        distances = {k: v['distance']['value'] for k,v in details.items()}
+        distances = {k: v["distance"]["value"] for k, v in details.items()}
         sorted_keys = sorted(distances, key=distances.get)
-        lowest, highest = helper_routes.safeget(details,sorted_keys[0],'distance','text'), helper_routes.safeget(details,sorted_keys[-1],'distance','text')
+        lowest, highest = helper_routes.safeget(
+            details, sorted_keys[0], "distance", "text"
+        ), helper_routes.safeget(details, sorted_keys[-1], "distance", "text")
         range = f"{lowest} - {highest}"
 
-        return render_template(
-            "routes.html",
-            distance_range = range,
-            details = details
-        )
+        return render_template("routes.html", distance_range=range, details=details)
