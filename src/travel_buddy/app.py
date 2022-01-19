20e59260035491fd618c5a90886b9ad45abe61f1
IsaacCheng9 <47993930+IsaacCheng9@users.noreply.github.com>
2022-01-19 17:31:39 +0000

    Add a function to create the Flask web app

--- src/travel_buddy/app.py
+++ src/travel_buddy/app.py
@@ -8,20 +8,27 @@ import travel_buddy.views.profile as profile
 import travel_buddy.views.register as register
 import travel_buddy.views.settings as settings
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "db.sqlite3")
 
-app = Flask(__name__)
-app.register_blueprint(register.register_blueprint, url_prefix="")
-app.register_blueprint(login.login_blueprint, url_prefix="")
-app.register_blueprint(profile.profile_blueprint, url_prefix="")
-app.register_blueprint(settings.settings_blueprint, url_prefix="")
-app.register_blueprint(carpool.carpool_blueprint, url_prefix="")
+def create_app() -> Flask:
+    """
+    Creates an instance of the Flask web application.
 
-app.url_map.strict_slashes = False
-app.secret_key = (
-    "i09kd0owe671gdyfjkzmpaopmrionfkwep]l[';fkvfoiejiojd1u232e23fkeorincuiwkdu"
-)
+    Returns:
+        An instance of the web application with the blueprints configured.
+    """
+    app = Flask(__name__)
+    app.register_blueprint(register.register_blueprint, url_prefix="")
+    app.register_blueprint(login.login_blueprint, url_prefix="")
+    app.register_blueprint(profile.profile_blueprint, url_prefix="")
+    app.register_blueprint(settings.settings_blueprint, url_prefix="")
+    app.register_blueprint(carpool.carpool_blueprint, url_prefix="")
+
+    app.url_map.strict_slashes = False
+    app.secret_key = (
+        "i09kd0owe671gdyfjkzmpaopmrionfkwep]l[';fkvfoiejiojd1u232e23fkeorincuiwkdu"
+    )
+    return app
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    create_app().run(debug=True)
