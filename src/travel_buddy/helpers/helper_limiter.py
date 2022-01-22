906f9683968097d0b31f11ea857df57355a74b44
IsaacCheng9 <47993930+IsaacCheng9@users.noreply.github.com>
2022-01-22 21:25:00 +0000

    Add rate limiter helper

---
+++ src/travel_buddy/helpers/helper_limiter.py
@@ -0,0 +1,7 @@
+"""
+Sets up rate limiting for the application.
+"""
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+limiter = Limiter(key_func=get_remote_address)
