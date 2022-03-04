6353c5334363b4a7b8bbde3d50f361fa83cc380d
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-03-04 02:33:06 +0000

    Use sliding window strategy for rate limiter

--- src/travel_buddy/helpers/helper_limiter.py
+++ src/travel_buddy/helpers/helper_limiter.py
@@ -4,4 +4,6 @@ Sets up rate limiting for the application.
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address, default_limits=["3/second"])
+limiter = Limiter(
+    key_func=get_remote_address, default_limits=["3/second"], strategy="moving-window"
+)
