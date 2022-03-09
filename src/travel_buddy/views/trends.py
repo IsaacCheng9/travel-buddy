2cadb1e1c454e24f8812461101b540c035036d30
BarnabasG <BarnabasG@users.noreply.github.com>
2022-03-09 00:45:57 +0000

    fix initial get request for trends

--- src/travel_buddy/views/trends.py
+++ src/travel_buddy/views/trends.py
@@ -4,7 +4,7 @@ Handles the view for long term trends and estimations for a user.
 import travel_buddy.helpers.helper_routes as helper_routes
 import travel_buddy.helpers.helper_general as helper_general
 
-from flask import Blueprint, redirect, render_template, request, session
+from flask import Blueprint, redirect, render_template, request, session, url_for
 from travel_buddy.helpers.helper_limiter import limiter
 
 trends_blueprint = Blueprint(
@@ -21,20 +21,14 @@ def show_trends():
         return redirect("/")
 
     car_make, car_mpg, fuel_type, engine_size = helper_routes.get_car(
-        session.get("username")
-    )
+            session.get("username")
+        )
+    
+    return redirect(
+            url_for('trends.show_car', car=car_make, mpg=car_mpg, fuel=fuel_type, engine=engine_size)
+        )
 
-    return render_template(
-        "trends.html",
-        username=session.get("username"),
-        car_make=car_make,
-        car_mpg=car_mpg,
-        fuel_type=fuel_type,
-        engine_size=engine_size,
-    )
-
-
-@trends_blueprint.route("/trends/add_car", methods=["GET"])
+@trends_blueprint.route("/trends/view_car", methods=["GET"])
 @limiter.limit("15/minute")
 def show_car():
     if "username" not in session:
