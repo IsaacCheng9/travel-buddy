0e7153e03ddb5ada798066ae078277caddf5dec1
BarnabasG <BarnabasG@users.noreply.github.com>
2022-03-15 18:12:30 +0000

    update procfile and wsgi

--- src/wsgi.py
+++
@@ -1,4 +0,0 @@
-import travel_buddy.app as app
-
-if __name__ == "__main__":
-    app.run()
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+import src.travel_buddy.app as app
+
+if __name__ == "__main__":
+    app.create_app().run(debug=True)
