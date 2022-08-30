e7f0fead47c9c758f681589321d97d6eb8cfd7dc
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2022-08-30 02:47:11 +0100

    Disable API key test, update README regarding this

--- README.md
+++ README.md
@@ -3,17 +3,21 @@
 [![code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 ![CI](https://github.com/IsaacCheng9/travel-buddy/actions/workflows/main.yml/badge.svg)
 
+**UPDATE (30th August 2022):** The route analysis and related features such as
+location autocomplete are currently unavailable as our key for the Google Maps
+API has expired.
+
 <img width="488" alt="image" src="https://user-images.githubusercontent.com/47993930/157455295-10e79608-0b29-4446-bc05-cf2e3cbfc662.png">
 
 A travel companion to help plan your journey while saving both money and the
 planet by providing route analysis and a carpool marketplace.
 
 ## Screenshots
+
 <img width="2056" alt="image" src="https://user-images.githubusercontent.com/47993930/160259708-872d9827-c011-4e3f-9492-13b3dd6876d9.png">
 <img width="2056" alt="image" src="https://user-images.githubusercontent.com/47993930/160259721-6ae304aa-d6da-4b1b-bef6-20968e55006d.png">
 <img width="2056" alt="image" src="https://user-images.githubusercontent.com/47993930/160259727-5bda8093-7a97-498a-b338-8806284647eb.png">
 <img width="2056" alt="image" src="https://user-images.githubusercontent.com/47993930/160259626-7fe98e35-d7b0-4a2e-abf8-a2faa9b0e492.png">
-
 
 ## Installation
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -19,23 +19,24 @@ def test_key_is_read():
     assert KEYS.get("test_key4") == "   "
 
 
-def test_api_key_is_valid():
-    """
-    Tests that the given encoded api keys can be decoded, the google maps client can be created,
-    and the keys can be used in with the google maps api to return correct data.
-    """
-    API_KEY_FILE = "keys.json"
-    KEYS = helper_general.get_keys(API_KEY_FILE)
+# !IMPPORTANT: Disabled the test indefinitely due to expired Google Maps API key.
+# def test_api_key_is_valid():
+#     """
+#     Tests that the given encoded api keys can be decoded, the google maps client can be created,
+#     and the keys can be used in with the google maps api to return correct data.
+#     """
+#     API_KEY_FILE = "keys.json"
+#     KEYS = helper_general.get_keys(API_KEY_FILE)
 
-    map_client = helper_routes.generate_client(KEYS["google_maps"])
+#     map_client = helper_routes.generate_client(KEYS["google_maps"])
 
-    assert map_client is not None
+#     assert map_client is not None
 
-    origin = "University of Exeter Forum Library, Stocker Rd, Exeter EX4 4PT"
-    destination = "Exeter Quay, Exeter EX2 4BZ"
+#     origin = "University of Exeter Forum Library, Stocker Rd, Exeter EX4 4PT"
+#     destination = "Exeter Quay, Exeter EX2 4BZ"
 
-    data = helper_routes.run_api(map_client, origin, destination, "walking")
+#     data = helper_routes.run_api(map_client, origin, destination, "walking")
 
-    assert data is not None
-    assert data.get("origin_addresses")[0] == "Stocker Rd, Exeter EX4 4PT, UK"
-    assert data.get("destination_addresses")[0] == "Exeter Quay, Exeter EX2 4BZ, UK"
+#     assert data is not None
+#     assert data.get("origin_addresses")[0] == "Stocker Rd, Exeter EX4 4PT, UK"
+#     assert data.get("destination_addresses")[0] == "Exeter Quay, Exeter EX2 4BZ, UK"
