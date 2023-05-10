fa078ad97cd1a5632aad3aafcc20b5cd884641ef
KPiatigorskii <kpiatigorskii@gmail.com>
2023-05-10 13:52:51 +0300

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ git clone https://github.com/KPiatigorskii/travel-buddy
 cd travel-buddy/
 python3 -m venv env
 source env/bin/activate
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
+pip3 install -e .
 sudo chown -R ec2-user:ec2-user /home/ec2-user/travel-buddy/src/travel_buddy.egg-info
 pip3 install -e .
     
