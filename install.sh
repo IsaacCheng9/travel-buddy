fa4d8449d38cb0a1e147bbcd41234c9c25e3c2a9
KPiatigorskii <kpiatigorskii@gmail.com>
2023-05-10 10:53:33 +0300

    add rules to script

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 sudo yum install -y python3-pip
+sudo yum install -y git
+cd /home/ec2-user/
+git clone https://github.com/KPiatigorskii/travel-buddy
 sudo pip3 install -r /home/ec2-user/travel-buddy/requirements.txt
     
